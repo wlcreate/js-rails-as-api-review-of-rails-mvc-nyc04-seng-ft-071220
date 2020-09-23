68e88ab0f4f57913ba3bb53c67d0f7a99ad689e8
wlcreate <waverley.nyc@gmail.com>
2020-09-23 17:23:09 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@ Bird.create(name: "Black-Capped Chickadee", species: "Poecile Atricapillus")
 Bird.create(name: "Grackle", species: "Quiscalus Quiscula")
 Bird.create(name: "Common Starling", species: "Sturnus Vulgaris")
 Bird.create(name: "Mourning Dove", species: "Zenaida Macroura")
+
+puts "🦉🦉🦉🦉🦉🦉"
